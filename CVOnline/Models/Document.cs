bce24e2c0cd0731ac01755f3e065482332dbfc4b
jatipandus <jatipandus@gmail.com>
2020-04-30 13:48:11 +0700

    Add document Model

---
+++ CVOnline/Models/Document.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CVOnline.Models
+{
+    [Table("TB_M_Document")]
+    public class Document
+    {
+        [Key]
+        public int Id { get; set; }
+        public string IdCard { get; set; }
+        public string Resume { get; set; }
+        public string CV { get; set; }
+        public string FamilyCard { get; set; }
+        public string Transcripts { get; set; }
+        public string Diploma { get; set; }
+        public string Certificate { get; set; }
+    }
+}
