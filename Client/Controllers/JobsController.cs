807bf60f9cbd7def052224c14dd2c88a33794430
Muhammad Karim <muhammadkarim10@gmail.com>
2020-05-03 23:51:45 +0700

    add load jobs client

---
+++ Client/Controllers/JobsController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using CVOnline.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Client.Controllers
+{
+    public class JobsController : Controller
+    {
+
+        private HttpClient client = new HttpClient
+        {
+            BaseAddress = new Uri("https://localhost:44348/api/")
+        };
+
+        public IActionResult Index()
+        {
+            return View(LoadJobs());
+        }
+
+
+        public JsonResult LoadJobs()
+        {
+            IEnumerable<Job> jobs = null;
+            var responseTask = client.GetAsync("Jobs");
+            responseTask.Wait();
+            var result = responseTask.Result;
+            if (result.IsSuccessStatusCode) 
+            {
+                var readTask = result.Content.ReadAsAsync<IList<Job>>(); 
+                readTask.Wait();
+                jobs = readTask.Result;
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Server Error");
+            }
+            return Json(jobs);
+        }
+    }
+}
---
+++ Client/wwwroot/Scripts/JobScripts.js
@@ -0,0 +1,27 @@
+﻿var table = null;
+$(document).ready(function () {
+    debugger;
+    table = $('#jt').dataTable({ //Nama table pada index
+        "ajax": {
+            url: "/Jobs/LoadJobs", 
+            type: "GET",
+            dataType: "json",
+            dataSrc: ""
+        },
+        "columnDefs": [
+            { "orderable": false, "targets": 3 },
+            { "searchable": false, "targets": 3 }
+        ],
+        "columns": [
+            { "data": "jobName" },
+            { "data": "isActive" },
+            { "data": "dueDate" },
+            {
+                data: null, render: function (data, type, row) {
+                    return '<button type="button" class="btn btn-warning" id="BtnEdit" data-toggle="tooltip" data-placement="top" title="Edit" onclick="return GetById(' + row.id + ')"><i class="mdi mdi-pencil"></i></button> &nbsp; <button type="button" class="btn btn-danger" id="BtnDelete" data-toggle="tooltip" data-placement="top" title="Delete" onclick="return Delete(' + row.id + ')"><i class="mdi mdi-delete"></i></button>';
+                }
+            }
+        ]
+    });
+
+});
